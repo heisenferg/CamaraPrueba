fe80aef8d53776fcb6112994aee68870d9f2f267
heisenferg <heisenferg@gmail.com>
2022-02-23 14:50:25 +0100

    3 con Thumbnail

--- app/src/main/java/com/example/camaraprueba/MainActivity.java
+++ app/src/main/java/com/example/camaraprueba/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.camaraprueba;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 public static int CAPTURA_IMAGEN=1;
+
+    public final int CAPTURA=0;
 Button cameraButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,15 @@ Button cameraButton;
             }
         });
 
-
+        requestPermissions(new String[]{"android.permission.CAMERA"},CAPTURA);
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 
     public void hacerFoto() {
         Intent hacerFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -33,4 +46,17 @@ Button cameraButton;
             startActivityForResult(hacerFotoIntent, CAPTURA_IMAGEN);
         }
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURA_IMAGEN && resultCode == RESULT_OK) {
+            ImageView v = (ImageView) findViewById(R.id.imageView);
+            Bundle extras = data.getExtras();
+            Bitmap imagen = (Bitmap) extras.get("data");
+            v.setImageBitmap(imagen);
+        }
+    }
+
 }
